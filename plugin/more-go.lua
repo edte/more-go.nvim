1698322c110fc2ae30b8df788074eeec485dac73
edte <zzzzip6@gmail.com>
2025-04-30 16:32:18 +0800

    init project

--- plugin/more-go.lua
+++ plugin/more-go.lua
@@ -1,7 +1,7 @@
-local cmp = require("cmp")
-if cmp ~= nil then
-	cmp.register_source("go_pkgs", require("more-go.go-pkgs-cmp"))
-end
+-- local cmp = require("cmp")
+-- if cmp ~= nil then
+-- 	cmp.register_source("go_pkgs", require("more-go.go-pkgs-cmp"))
+-- end
 
 require("more-go.go-pkgs-blink").setup({})
 
